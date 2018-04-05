483eedcb0e635020caa881f19003e2d7386eaa47
Kala Boisvert <kalaaboisvert@gmail.com>
2018-04-05 18:38:04 -0400

    first commit

---
+++ april3rd.js
@@ -0,0 +1,10 @@
+//console.log(document.querySelector)('p') ;
+
+
+
+
+var Button1 = document.getElementById('one');
+Button1.addEventListener('click', function() { alert('there are three paragraph elements in this id') })
+
+//alert.(document.getElementsById)('one')
+//alert.(document.getElementsById)('three')
